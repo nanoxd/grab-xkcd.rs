951cc8e137139d5f0af511c205a32b01dc8593ab
Fernando Paredes <nano@fdp.io>
2020-07-05 12:41:49 -0400

    Parse CLI Options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Clap;
+
+#[derive(Debug, Clap)]
+pub struct Options {
+    /// Sets a connection timeout
+    #[clap(long, short, default_value = "60")]
+    pub timeout: u64,
+
+    /// Prints output in the given format
+    #[clap(long, short, arg_enum, default_value = "text")]
+    pub output: OutputFormat,
+
+    /// The comic to loads
+    #[clap(long, short, default_value = "0")]
+    pub num: usize,
+
+    /// Save image file to current directory
+    #[clap(long, short)]
+    pub save: bool,
+}
+
+#[derive(Debug, Clap, Copy, Clone)]
+pub enum OutputFormat {
+    Json,
+    Text,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Options::parse();
+    println!("{:?}", &args);
 }
